58fed759fe00c420a2f1155cebf3a702a67035e5
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2023-03-09 09:02:01 +0530

    commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String code = "create table mytab 2\nint a\nstring b\ncreate table mytab2 3\nint a\nstring b\nstring c\ncreate table mytab3 4\nint a1\nstring b1\nstring c1\nint d1\ninsert into mytab (12,23.4)";
+
+        List<String> intermediateCode = cs20b030_parser.parse(code);
+        System.out.println(intermediateCode.size());
+        for (String line : intermediateCode) {
+            System.out.println(line);
+        }
     }
 }
---
+++ src/cs20b030_dbengine.java
@@ -0,0 +1,3 @@
+public class cs20b030_dbengine {
+
+}
